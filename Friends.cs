fd73002a4522a660629fad9016a41378ee147b67
Sárdi András <sardi.andras@gmail.com>
2022-10-09 15:01:18 +0200

    Create Friends.cs

---
+++ Friends.cs
@@ -0,0 +1,6 @@
+string firstFriend = "Bill";
+string secondFriend = "John";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
