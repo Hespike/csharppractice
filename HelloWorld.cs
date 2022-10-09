8ed4e746c101bac7139f813c81bc5b2f21e13bcd
Sárdi András <sardi.andras@gmail.com>
2022-10-09 14:21:49 +0200

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
