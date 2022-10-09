72292c4e984c03a2aff3c3e63e10318ea5c9a48b
Sárdi András <sardi.andras@gmail.com>
2022-10-09 14:02:55 +0200

    Create Alive.cs

---
+++ Alive.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class Alive
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("I'm alive");
+        }
+    }
+}
