29b6a2a0ad8d6958bd0a704e8bfb4f8338b241b5
Sárdi András <sardi.andras@gmail.com>
2022-10-11 16:08:20 +0200

    Create HelloName.cs

---
+++ HelloName.cs
@@ -0,0 +1,9 @@
+//Write a C# Sharp program to print Hello and your name in a separate line.
+
+public class Exercise1
+{
+    public static void Main( )
+    {
+         System.Console.WriteLine("Hello: \nHespi");
+    }
+}
