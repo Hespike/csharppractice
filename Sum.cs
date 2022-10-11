9b7ff7762233486825350184360bc1c837bec20d
Sárdi András <sardi.andras@gmail.com>
2022-10-11 16:09:51 +0200

    Create Sum.cs

---
+++ Sum.cs
@@ -0,0 +1,11 @@
+//  Write a C# Sharp program to print the sum of two numbers.
+
+using System;
+
+public class Exercise1
+{
+    public static void Main( )
+    {
+         Console.WriteLine(1+1);
+    }
+}
