0d87df47627bb15815797aa353340d73f0459066
Sárdi András <sardi.andras@gmail.com>
2022-10-11 16:34:31 +0200

    Create Divide.cs

---
+++ Divide.cs
@@ -0,0 +1,9 @@
+// Write a C# Sharp program to print the result of dividing two numbers.
+
+public class Exercise1
+{
+    public static void Main( )
+    {
+         System.Console.WriteLine(4/2);
+    }
+}
